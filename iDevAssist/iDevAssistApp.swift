8b8e4e8c055c165d1a82e5def84c0d3f7a38a5a0
Abhijith PM <pmabi1993@outlook.com>
2024-05-22 13:23:53 +0800

    Initial Commit

---
+++ iDevAssist/iDevAssistApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDevAssistApp.swift
+//  iDevAssist
+//
+//  Created by Abhijith Pm on 22/05/24.
+//
+
+import SwiftUI
+
+@main
+struct iDevAssistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iDevAssistUITests/iDevAssistUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  iDevAssistUITestsLaunchTests.swift
+//  iDevAssistUITests
+//
+//  Created by Abhijith Pm on 22/05/24.
+//
+
+import XCTest
+
+final class iDevAssistUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
